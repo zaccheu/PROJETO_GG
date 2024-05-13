278eaa1d35a853b80a614519f28f76958b96be22
Lucas Germinari <carreiralgc@gmail.com>
2024-05-13 18:05:03 -0400

    Arquivos para controller e repository do Pratos criados.

---
+++ Controllers/PratoController.cs
@@ -0,0 +1,23 @@
+﻿/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+//* Autor(es): 
+//* Data da última modificação: 13/05/2024
+//* Descrição: Controller para a entidade Prato
+//* Testes: 
+//* Anotações:
+    - "Pratos" ou "Produtos"?
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+
+namespace CadastroClientes.Controllers
+{
+    public class Prato
+    {
+    }
+}
