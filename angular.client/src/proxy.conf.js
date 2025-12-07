0ed8f37676c448a2a39dc20e63d0204efca18630
zaccheu <gu.zaccheu8@gmail.com>
2025-12-07 02:09:14 -0300

    exclusão projeto angular junto da solução antiga

--- angular.client/src/proxy.conf.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  context: ['/api'], // Adjust context paths as needed
-  target: 'https://localhost:44344', // Replace with your backend API URL
-  secure: true, // Set to true if using HTTPS for the backend API
-  changeOrigin: true, // Set to false if backend doesn't support CORS
-  pathRewrite: { '^/api': '' } // Adjust path rewrite rules as needed
-};
